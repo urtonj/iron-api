96ffa83db4328fe675ad91d6a44faaf2551a2fed
Jason Urton <jason.urton@thelevelup.com>
2015-10-25 11:53:07 -0400

    Switch database to Postgres

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20151024233900) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "exercise_group_exercises", force: :cascade do |t|
     t.integer "exercise_id"
     t.integer "exercise_group_id"
