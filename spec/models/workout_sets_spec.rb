6c103fa871483bcc0a99fe1ceb157fb80b08ee70
Jason Urton <jason.urton@gmail.com>
2015-11-05 00:08:56 -0500

    Include FactoryGirl helpers

--- spec/models/workout_sets_spec.rb
+++ spec/models/workout_sets_spec.rb
@@ -9,13 +9,13 @@ describe WorkoutSet do
 
   context '#previous_set' do
     before do
-      user = FactoryGirl.create(:user)
-      exercise = FactoryGirl.create(:exercise)
-      workout = FactoryGirl.create(:workout, user_id: user.id)
-      FactoryGirl.create :workout_set, exercise_id: exercise.id, workout_id: workout.id
-      @previous_set = FactoryGirl.create(:workout_set, exercise_id: exercise.id,
+      user = create(:user)
+      exercise = create(:exercise)
+      workout = create(:workout, user_id: user.id)
+      create :workout_set, exercise_id: exercise.id, workout_id: workout.id
+      @previous_set = create(:workout_set, exercise_id: exercise.id,
         workout_id: workout.id)
-      @new_set = FactoryGirl.create(:workout_set, exercise_id: exercise.id, workout_id: workout.id)
+      @new_set = create(:workout_set, exercise_id: exercise.id, workout_id: workout.id)
     end
 
     it 'returns the previous set' do
