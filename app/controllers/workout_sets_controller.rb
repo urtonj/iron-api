72d13b77dbb2e632e2a36b739783f34786c6b70e
Jason Urton <jason.urton@gmail.com>
2015-11-08 13:38:50 -0500

    Enable editing of sets

--- app/controllers/workout_sets_controller.rb
+++ app/controllers/workout_sets_controller.rb
@@ -5,13 +5,26 @@ class WorkoutSetsController < ApplicationController
     redirect_to workout_path set.workout_id
   end
 
+  def edit
+    @set = WorkoutSet.find(params[:id])
+    @set_for_autofill = @set
+  end
+
   def new
     @set = WorkoutSet.new(workout_set_params)
+    @set_for_autofill = @set.previous_set
+  end
+
+  def update
+    set = WorkoutSet.find(params[:id])
+    set.update workout_set_params
+
+    redirect_to workout_path set.workout_id
   end
 
   private
 
   def workout_set_params
-    params.require(:workout_set).permit(:exercise_id, :workout_id, :rep_count, :weight)
+    params.require(:workout_set).permit :exercise_id, :workout_id, :rep_count, :weight
   end
 end
